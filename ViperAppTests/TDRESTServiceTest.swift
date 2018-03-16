e194540b9dfa6f794e3f7a47c38868ec32cf6969
Romson Preechawit <r.preechawit@hotmail.com>
2018-03-16 22:03:25 +0700

    TDRESTServiceTest - Starting TDD: testGetAllProjects() now failed asynchronously!

--- ViperAppTests/TDRESTServiceTest.swift
+++ ViperAppTests/TDRESTServiceTest.swift
@@ -7,9 +7,42 @@
 //
 
 import XCTest
+import Mockingjay
 @testable import ViperApp
 
 class TDRESTServiceTest: XCTestCase {
+    
+    let MOCK_PROJECTS_JSON =
+        [
+            [
+                "id": "177723366",
+                "name": "Inbox",
+                "order": 0,
+                "indent": 1,
+                "comment_count": 0
+            ],
+            [
+                "id": "177723369",
+                "name": "Work",
+                "order": 1,
+                "indent": 1,
+                "comment_count": 0
+            ],
+            [
+                "id": "178149702",
+                "name": "University",
+                "order": 2,
+                "indent": 1,
+                "comment_count": 0
+            ],
+            [
+                "id": "2160755645",
+                "name": "SeniorProject",
+                "order": 4,
+                "indent": 2,
+                "comment_count": 0
+            ]
+        ]
     
     // This is an Implicitly Unwrapped Optionals.
     // The Todolist Service which we will initialzed in setUp()
@@ -34,7 +67,24 @@ class TDRESTServiceTest: XCTestCase {
     // ============================================
     
     func testGetAllProjects() {
+        stub(uri(TodolistAPI.projects.url), json(MOCK_PROJECTS_JSON))
         
+        let expectation = XCTestExpectation(description: "Expect funtion to return a list of TDProjects")
+        
+        var expectedResult = [TDProject]()
+        expectedResult.append(TDProject.init(id: "177723366", name: "Inbox", indent: 1, order: 0))
+        expectedResult.append(TDProject.init(id: "177723369", name: "Work", indent: 1, order: 1))
+        expectedResult.append(TDProject.init(id: "178149702", name: "University", indent: 1, order: 2))
+        expectedResult.append(TDProject.init(id: "2160755645", name: "SeniorProject", indent: 2, order: 4))
+        
+        
+        tdService.getAllProjects(nil) { projects in
+            XCTAssert( expectedResult.count == projects.count)
+            expectation.fulfill()
+        }
+        
+        // wait for Async task, since we stub the request, timeout can be low
+        wait(for: [expectation], timeout: 1.0)
     }
     
     func testGetAllLabels() {
