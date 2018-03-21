40058062efd76c8e6d7899de8680af751dbb28d7
Romson Preechawit <r.preechawit@hotmail.com>
2018-03-21 16:30:38 +0700

    Fix bug in TDOAuthService when the completion may be called before Safari is fully dismissed, causing showDashboard() to failed.

--- ViperApp/Supporting/TodoistModule/Services/TDOAuthService.swift
+++ ViperApp/Supporting/TodoistModule/Services/TDOAuthService.swift
@@ -26,52 +26,75 @@ class TDOAuthService: OAuthService {
     
     func initiateOAuth(displayOAuthPageOn view: UIViewController, error: ((AnyObject?) -> Void)?, success: @escaping (String) -> Void) {
         
-        let state = generateState(withLength: 10)
-        
+        // set handler to use an internal Safari WebKit
         let handler = SafariURLHandler(viewController: view, oauthSwift: oauthswift)
+        
+        // OAuthSwift call success(...) completion handler
+        // asynchronously with the handler.dismissCompletion.
+        // This sometime causes problem where success(...) may
+        // fires before the SafariURLHandler finish dismissing
+        // which causes problem when trying to present a view
+        // after the login is complete.
+        
+        // This dispatchGroup make sure that both completion
+        // handlers is completed before notifying the completion
+        // handler given by the user of this function
+        let dispatchGroup = DispatchGroup()
+        
+        // a token variable to capture oauthtoken string from
+        // OAuthSwift's compltion handler before passing it
+        // back upon DispatchGroup finishing.
+        var token: String?
+        
+        // Enter dispatch group when presenting Safari and leave
+        // when fully dismissed.
         handler.presentCompletion = {
-            print("Safari presented")
+            dispatchGroup.enter()
         }
         handler.dismissCompletion = {
-            print("Safari dismissed")
+            dispatchGroup.leave()
         }
         
         oauthswift.authorizeURLHandler = handler
+        
+        // Initiate OAuth2 flow using OAuthSwift.
+        // Enter the dispatch once more for our Authorize flow.
+        dispatchGroup.enter()
+        
+        // generate a random string to verify integrity of our response
+        let state = generateState(withLength: 10)
+        
         let _ = oauthswift.authorize(
             withCallbackURL: URL(string: "com.rwp.tddashboard://oauth-callback")!,
             scope: "data:read",
             state: state,
             success: { credential, response, parameters in
-                success(credential.oauthToken)
+                token = credential.oauthToken
+                // Tell dispatchGroup we have received and stored the token.
+                dispatchGroup.leave()
             },
             failure: { error in
-                // TODO: handle error properly
+                // TODO: tell the user about the Error
                 print(error.errorCode)
                 print(error.errorUserInfo)
                 print(error.localizedDescription)
+                dispatchGroup.leave()
             }
         )
         
+        // Once we have received the token AND the handler is completely
+        // dismissed, we then execute completion handlers given by the
+        // user of this function.
+        dispatchGroup.notify(queue: .main) {
+            if let token = token {
+                success(token)
+            } else {
+                // TODO: capture and forward error
+                error?(nil)
+            }
+        }
+        
         
     }
-    
-    init() {
-        
-        
-        
-//        // TodoistiCal OAuth
-//        let oauth2settings = [
-//            "client_id": "6708fc59ee7f4746a6ea4ef13f660585",
-//            "client_secret": "0cee40bbdcbf4d38bf68b0ff9caaf8ac", // Todoist Client Secret
-//            "state": STATE, // From Todoist: A unique and unguessable string.
-//                            // It is used to protect you against cross-site request forgery attacks.
-//            "authorize_uri": "https://todoist.com/oauth/authorize",
-//            "token_uri": "https://todoist.com/oauth/access_token",
-//            "redirect_uris": "com.rwp.tddashboard://oauth/callback",
-//            "scope": "data:read", // we only need to read data
-//            ]
-        
-    }
-    
     
 }
